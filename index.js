0489f4de8374e7567450ff4e8e5b9bdf5ea331dd
ju3tin <ju3tin@hotmail.co.uk>
2024-04-24 22:02:38 +0100

    ,.,.,jjjj

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+
+// Serve the "bullshit" folder as a static directory
+app.use('/', express.static(path.join(__dirname, 'bullshit')));
+
+
 app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname,"/shit/index.html"));
 })
@@ -9,7 +14,7 @@ app.get('/', function (req, res) {
 
 
 
-var server = app.listen(5000, function () {
+var server = app.listen(8080, function () {
    
    console.log("Express App running at http://127.0.0.1:5000/");
 })
