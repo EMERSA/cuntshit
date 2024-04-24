ce5856a9290fff095de945ee77245360459ecee6
ju3tin <ju3tin@hotmail.co.uk>
2024-04-24 18:05:10 +0100

    fdgdgf

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname,"/shit/index.html"));
+})
+
+
+
+
+var server = app.listen(5000, function () {
+   
+   console.log("Express App running at http://127.0.0.1:5000/");
+})
